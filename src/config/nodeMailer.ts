9b086ae2b08b76d4f08770c8292c181988ce79f9
Tolomatico <ballesty.t@gmail.com>
2024-09-16 03:27:16 -0300

    mails added

--- src/config/nodeMailer.ts
+++ src/config/nodeMailer.ts
@@ -5,9 +5,8 @@ dotenv.config()
 
 const config = () => {
 
-    console.log(process.env.SMPT_HOST)
     return {
-        host: process.env.SMPT_HOST,
+        service: process.env.SMPT_HOST,
         port: +process.env.SMPT_PORT,
         auth: {
             user: process.env.SMPT_USER,
